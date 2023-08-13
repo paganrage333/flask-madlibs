6bdfe4075c287237aa70dcc87f8e6986cc7102e5
paganrage333 <aragan@unca.edu>
2023-08-13 16:22:11 -0400

    format

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, request, render_template
 from random import choice
-from stories import story 
+from stories import story
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def show_promts():
-    
+
     prompts = story.prompts
     return render_template('questions.html', prompts=prompts)
+
 
 @app.route('/home')
 def show_form():
     return render_template('form.html')
+
 
 @app.route('/story')
 def show_story():
